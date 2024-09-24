1029c8699c13a1284e8c30509a7692f1599367e4
Kami <kamdin@web.de>
2024-09-24 16:00:24 +0200

    t

---
+++ tag_4_minispiel/src/components/TextStarWars.tsx
@@ -0,0 +1,17 @@
+import Spoiler from "./Spoiler"
+
+
+const Text = () => {
+  return (
+    <p>
+    Star Wars (englisch für <Spoiler>Sternkriege</Spoiler>) ist ein Film-Franchise, dessen
+    Geschichte mit dem <Spoiler>1977</Spoiler> erschienenen Kinofilm Krieg der Sterne
+    (Originaltitel: Star Wars) begann. Schöpfer von Star Wars ist der
+    Drehbuchautor, Produzent und Regisseur George Lucas. Im Jahr 2012
+    verkaufte Lucas seine Firma <Spoiler>Lucasfilm</Spoiler>, mitsamt den Rechten an Star Wars,
+    an die Walt Disney Company.
+  </p>
+  )
+}
+
+export default Text
